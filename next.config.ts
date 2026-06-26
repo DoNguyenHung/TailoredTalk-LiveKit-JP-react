854c2ace257f124ba38100bff213161b3aa5cda1
Hung Do <nguyenhung_do@hotmail.com>
2026-06-26 19:26:02 -0400

    Ignore ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['streamdown'],
+  reactStrictMode: false,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
